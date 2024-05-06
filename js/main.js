cd564f1e991a66855fa01457b52988916cff9a33
Brina <brinamotto@gmail.com>
2024-05-06 22:23:33 +0200

    [add] const dividedNumber

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const firstName = prompt('Inserisci il tuo nome');
 const lastName = prompt('Inserisci il tuo cognome');
 const favouriteColor = prompt('Dimmi il tuo colore preferito!');
+const firstNumber = prompt('Il tuo numero fortunato è...');
+const secondNumber = prompt('E il tuo numero sfortunato?');
+const dividedNumber = Math.floor(firstNumber / secondNumber);
 const unsafePassword = `
 
 --------- La tua password supersicura è:------------
 
-${firstName}${lastName}${favouriteColor}21
+${firstName}${lastName}${favouriteColor}${dividedNumber}
 
 ----------------------------------------------------
 
