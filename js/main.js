e2bf9d9fc050c69d576c38412302203afdff4fdd
Brina <brinamotto@gmail.com>
2024-05-06 23:19:34 +0200

    [add] getElementById

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
+
 const firstName = prompt('Inserisci il tuo nome');
 const lastName = prompt('Inserisci il tuo cognome');
 const favouriteColor = prompt('Dimmi il tuo colore preferito!');
 const firstNumber = prompt('Il tuo numero fortunato è...');
 const secondNumber = prompt('E il tuo numero sfortunato?');
 const dividedNumber = Math.floor(firstNumber / secondNumber);
-const unsafePassword = `
-
---------- La tua password supersicura è:------------
-
-${firstName}${lastName}${favouriteColor}${dividedNumber}
-
-----------------------------------------------------
-
-`;
+const unsafePassword = `${firstName}${lastName}${favouriteColor}${dividedNumber}`;
 
 console.log(unsafePassword);
+
+document.getElementById('password').innerHTML = unsafePassword;
